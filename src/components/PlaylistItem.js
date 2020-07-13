33d30e1089b15b55332da5186cefb4e1efffd716
ducdeptrai1999 <53177120+minhduc@users.noreply.github.com>
2020-07-13 16:04:12 +0700

    first commit

---
+++ src/components/PlaylistItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StyledPlaylistitem from './styles/StyledPlaylistItem';
+
+const PlaylistItem = ({ video, active, played }) => (
+    <StyledPlaylistitem active={active} played={played}>
+        <div className="wbn-player__video-nr">{video.num}</div>
+        <div className="wbn-player__video-name">{video.title}</div>
+        <div className="wbn-player__video-time">{video.duration}</div>
+    </StyledPlaylistitem>
+)
+
+export default PlaylistItem;
